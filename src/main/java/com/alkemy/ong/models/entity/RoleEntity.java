6fc6c6d0a22312dcbae2649a6e82adef1e1dbc18
FacuDalessio <fdalessio22@gmail.com>
2022-06-16 11:33:59 -0300

    creation Role Entity

---
+++ src/main/java/com/alkemy/ong/models/entity/RoleEntity.java
@@ -0,0 +1,36 @@
+package com.alkemy.ong.models.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.annotations.GenericGenerator;
+
+@AllArgsConstructor
+@Table(
+        name = "roles"
+)
+@NoArgsConstructor
+@Data
+@Entity
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String Id;
+    
+    @NonNull
+    @Column(nullable = false)
+    private String name;
+    
+    private String description;
+    
+    private Timestamp timestamp;
+}
