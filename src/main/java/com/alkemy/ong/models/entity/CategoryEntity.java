2773aec2eacd3e1d02f4b3529090799732f80246
Franco adrian <adriantonanes@gmail.com>
2022-06-16 00:50:08 -0300

    CategoryEntity is been added

---
+++ src/main/java/com/alkemy/ong/models/entity/CategoryEntity.java
@@ -0,0 +1,40 @@
+package com.alkemy.ong.models.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.sql.Timestamp;
+
+@Table(name = "categories")
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+@SQLDelete(sql = "UPDATE categories SET soft_delete = true WHERE category_id=?")
+@Where(clause = "soft_delete = false")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @NotBlank(message = "Name can't be empty or void.")
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private String image;
+
+    @CreationTimestamp
+    @Column
+    private Timestamp timestamp;
+
+    @Column(name = "soft_delete")
+    private Boolean softDelete = false;
+}
