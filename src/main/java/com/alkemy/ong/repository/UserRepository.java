71365c8beb4aa16739b44bf1bb1ac41810cd43e8
caobandoc <obandocarlosandres@gmail.com>
2022-06-15 21:09:34 -0500

    Add annotation repository in UserRepository

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.models.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
 }
