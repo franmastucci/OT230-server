31e572dc492aeae85a83336897e37c70ab0e2e65
Adrian Olij <adrix.43@gmail.com>
2022-06-16 12:21:35 -0300

    OrganizationEntity and Reposotory Created

---
+++ src/main/java/com/alkemy/ong/repository/OrganizatrionRepository.java
@@ -0,0 +1,7 @@
+package com.alkemy.ong.repository;
+
+import com.alkemy.ong.models.entity.OrganizationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizatrionRepository extends JpaRepository<OrganizationEntity, Long> {
+}
