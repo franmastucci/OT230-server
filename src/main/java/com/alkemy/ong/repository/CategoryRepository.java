c9056ac2ae5f01fcf8e9e6504f4c9a9630009694
Franco adrian <adriantonanes@gmail.com>
2022-06-16 01:17:39 -0300

    CategoryRepository error fixed

--- src/main/java/com/alkemy/ong/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/repository/CategoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Long, CategoryEntity> {
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 }
