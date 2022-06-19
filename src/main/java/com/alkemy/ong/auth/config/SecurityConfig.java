ca4d0fecd0b06c7b96d2a280487c7ff0e0663b12
caobandoc <obandocarlosandres@gmail.com>
2022-06-18 23:24:35 -0500

    Endpoint for login

--- src/main/java/com/alkemy/ong/auth/config/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/auth/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.alkemy.ong.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,7 +20,7 @@ import com.alkemy.ong.auth.filter.JwtFilter;
 import com.alkemy.ong.auth.service.impl.UserDetailsServiceImpl;
 
 
-Configuration
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -27,50 +29,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	JwtFilter jwtRequestFilter;
-	
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
-    /*  
-    Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-           .antMatchers("/auth/register")
-           .antMatchers("/users/**");
-    }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf();
-        http.authorizeRequests()
-           .antMatchers("/auth/register").permitAll()
-           .antMatchers("/users/**").permitAll();
+    public void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+        managerBuilder.userDetailsService(myUserDetailsService);
     }
-     */
-    @Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(myUserDetailsService);
-	}
     
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
     	return super.authenticationManagerBean();
     }
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+				.antMatchers("/auth/register","/auth/login")
+				.antMatchers("/users/**");
+	}
     
     @Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-		.csrf()
-		.disable()
-		.authorizeRequests()
-		.antMatchers(HttpMethod.POST, "/auth/register").permitAll()
-		.antMatchers(HttpMethod.POST, "/auth/login").permitAll()
-		.antMatchers("/users/**").permitAll()
-		.anyRequest().authenticated()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.csrf()
+				.disable()
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/auth/register", "/auth/login").permitAll()
+				.antMatchers("/users/**").permitAll()
+				.anyRequest().authenticated()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class).exceptionHandling()
 		.authenticationEntryPoint(new Http403ForbiddenEntryPoint());;
