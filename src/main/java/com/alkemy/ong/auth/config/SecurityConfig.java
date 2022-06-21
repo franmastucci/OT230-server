dd8e29dc518a7dd914d653cf67d0d72ff464b9d9
Fran <franmastucci@gmail.com>
2022-06-21 20:30:52 -0300

    public endpoint array added

--- src/main/java/com/alkemy/ong/auth/config/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/auth/config/SecurityConfig.java
@@ -48,6 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
     	return super.authenticationManagerBean();
     }
+    
+	private static final String[] publicEndpoint = {
+			//Swagger routes and docs
+	};
 
 
 	@Override
