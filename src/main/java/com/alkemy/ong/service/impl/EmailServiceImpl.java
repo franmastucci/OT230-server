5667c819a0f990b1cfc4ee369ae02f5018d3f4b1
Adrian Olij <adrix.43@gmail.com>
2022-06-21 22:53:49 -0300

    merging from remote

--- src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/EmailServiceImpl.java
@@ -2,39 +2,39 @@ package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.service.EmailService;
 import com.sendgrid.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-
+@Service
 public class EmailServiceImpl implements EmailService {
-
-    @Autowired
-    private Environment env;
 
     @Value("${alkemy.ong.email.sender}")
     private String emailSender;
 
+    @Value("$alkemy.ong.email.apikey")
+    private String apiKey;
+
+    @Value(("$alkemy.ong.email.templateid"))
+    private String templateID;
+
     @Override
-    public void sendEmailTo(String to) {
+    public void sendEmailTo(String to){
 
-        String apiKey = env.getProperty("EMAIL_API_KEY");
-
-        Email fromEmail = new Email(emailSender);
         Email toEmail = new Email(to);
-        Content content = new Content("text/css", "body")
+        Email fromEmail = new Email(emailSender);
+        Content content = new Content("text/html", "body");
+        String subject = "Somos Mas ONG y te damos la bienvenida!";
+        Mail mail = new Mail(fromEmail, subject, toEmail, content);
 
+        mail.setTemplateId(templateID);
 
-        String subjetc = "Somos Mas ONG y te damos la bienvenida!";
-
-        Mail mail = new Mail(fromEmail, subjetc, toEmail, content);
         SendGrid sg = new SendGrid(apiKey);
         Request request = new Request();
-        try{
+        try {
             request.setMethod(Method.POST);
-            request.setEndpoint("mail/welcome");
+            request.setEndpoint("mail/send");
             request.setBody(mail.build());
             Response response = sg.api(request);
 
@@ -44,6 +44,5 @@ public class EmailServiceImpl implements EmailService {
         } catch (IOException ex) {
             System.out.println("Error trying to send Welcome Email");
         }
-
     }
 }
