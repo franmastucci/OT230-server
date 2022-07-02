f413bcfe5c54d58fc87294b402a756715f7ab679
Adrian <adrix.43@gmail.com>
2022-07-01 21:46:29 -0300

    Moder, Repository and Service Created

---
+++ src/main/java/com/alkemy/ong/models/entity/CommentEntity.java
@@ -0,0 +1,31 @@
+package com.alkemy.ong.models.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "co")
+public class CommentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @NotBlank(message = "The content can't be blank")
+    @NotNull(message = "The content can't be null")
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    @Column(name = "news_id")
+    private Long newsId;
+}
