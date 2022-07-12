2b93b639032b5be77af4f8fd0c378cd53207e4fb
Facundo Dalessio <Fdalessio22@gmail.com>
2022-07-12 20:16:25 -0300

    Swagger

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -2,7 +2,14 @@ package com.alkemy.ong.controller;
 
 import javax.validation.Valid;
 
+import com.alkemy.ong.models.response.NewsPageResponse;
+import com.alkemy.ong.models.response.NewsResponse;
+import com.alkemy.ong.models.response.UserResponse;
 import com.alkemy.ong.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +25,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/news")
+@Api(value = "News Controller", description = "Crud news, list, search by id and search comments for news")
 public class NewsController {
 	
 	@Autowired
@@ -28,6 +36,9 @@ public class NewsController {
 
 	@PreAuthorize("hasRole('ROLE_ADMIN') OR hasRole('ROLE_USER')")
 	@GetMapping()
+	@ApiOperation(value = "List news",
+				notes = "Return a news list")
+	@ApiResponse(code = 200, message = "OK")
 	ResponseEntity<?> getAllNews(@RequestParam Optional<Integer> page){
 		if (page.isPresent()){
 			return new ResponseEntity<>(newsServ.getPaginationNews(page.get()), HttpStatus.OK);
@@ -38,30 +49,48 @@ public class NewsController {
 	
 	@PreAuthorize("hasRole('ROLE_ADMIN') OR hasRole('ROLE_USER')")
 	@GetMapping(path = "/{id}")
+	@ApiOperation(value = "Find By Id", notes = "Return a NewsResponse")
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "OK"),
+		@ApiResponse(code = 404, message = "NEWS NOT FOUND")
+	})
 	ResponseEntity<?> getNews(@PathVariable("id") @Valid @NonNull Long id){
 		return new ResponseEntity<>(newsServ.getNewsById(id), HttpStatus.OK);
 	}
-	
+
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping()
-	ResponseEntity<?> getNews(@Valid @RequestBody NewsRequest news){
+	@ApiOperation(value = "Create news",
+			notes = "Return a newsResponse",
+			response = NewsResponse.class)
+	@ApiResponse(code = 200, message = "CREATED")
+	ResponseEntity<?> createNews(@Valid @RequestBody NewsRequest news){
 		return new ResponseEntity<>(newsServ.createNews(news), HttpStatus.CREATED);
 	}
-	
+
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PutMapping(path = "/{id}")
+	@ApiOperation(value = "Update news",
+			notes = "Return a newsResponse")
+	@ApiResponse(code = 200, message = "OK")
 	ResponseEntity<?> putNews(@Valid @PathVariable("id") Long id, @RequestBody NewsRequest news){
 		return new ResponseEntity<>(newsServ.updateNews(id, news), HttpStatus.OK);
 	}
 	
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@DeleteMapping(path = "/{id}")
+	@ApiOperation(value = "Delete news",
+			notes = "Return a confirmation")
+	@ApiResponse(code = 200, message = "OK")
 	ResponseEntity<?> deleteNews(@Valid @PathVariable("id") Long id){
 		return new ResponseEntity<>(newsServ.removeNews(id), HttpStatus.OK);
 	}
 	
 	@PreAuthorize("hasRole('ROLE_ADMIN') OR (hasRole('ROLE_USER'))")
 	@GetMapping(path = "/{id}/comments")
+	@ApiOperation(value = "Find coments by news Id",
+			notes = "Return a coment list")
+	@ApiResponse(code = 200, message = "OK")
 	ResponseEntity<?> getCommentsByNewsId(@Valid @PathVariable("id") Long id){
 		return new ResponseEntity<>(commentServ.findCommentsByNews(id), HttpStatus.OK);
 	}
