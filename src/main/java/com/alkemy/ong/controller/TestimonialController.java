61926d0fa332b987595e28b4513bbd4acdc397cb
Adrian Olij <adrix.43@gmail.com>
2022-07-13 05:51:21 -0300

    testimonial pagination and start document

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -1,8 +1,12 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.models.request.TestimonialRequest;
+import com.alkemy.ong.models.response.TestimonialPageResponse;
 import com.alkemy.ong.models.response.TestimonialResponse;
 import com.alkemy.ong.service.TestimonialService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +16,10 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 import static com.alkemy.ong.utils.ApiConstants.ROLE_ADMIN;
+import static com.alkemy.ong.utils.ApiConstants.ROLE_USER;
 
 @RestController
+@Api(value = "Testimonial controller", description = "CRUD and Gets methods pertaining to Testimonials")
 @RequestMapping(path = "/testimonials")
 @PreAuthorize(ROLE_ADMIN)
 public class TestimonialController {
@@ -22,6 +28,9 @@ public class TestimonialController {
     private TestimonialService testimonialService;
 
     @PostMapping
+    @ApiOperation(value = "Create a Testimonial", notes = "Create a Testimonial using TestimonialRequest save in DB " +
+            "and return a TestimonialResponse")
+    @ApiResponse(code = 201, message = "CREATED")
     public ResponseEntity<TestimonialResponse> createTestimonial(
                                                         @Valid @RequestBody TestimonialRequest testimonial){
         return new ResponseEntity<>(testimonialService.createTestimonial(testimonial), HttpStatus.CREATED);
@@ -39,7 +48,12 @@ public class TestimonialController {
     public ResponseEntity<String> deleteTestimonial(@PathVariable Long id){
 
         testimonialService.deleteTestimonial(id);
-
         return new ResponseEntity<>("Testemional deleted", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/get-all")
+    @PreAuthorize(ROLE_USER)
+    public ResponseEntity<TestimonialPageResponse> getTestimonials(@RequestParam(defaultValue = "1") Integer page) {
+        return ResponseEntity.ok(testimonialService.pagination(page));
     }
 }
--- src/main/java/com/alkemy/ong/service/impl/TestimonialServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/TestimonialServiceImpl.java
@@ -1,17 +1,25 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.exception.OrgNotFoundException;
+import com.alkemy.ong.models.entity.MemberEntity;
 import com.alkemy.ong.models.entity.TestimonialEntity;
 import com.alkemy.ong.models.mapper.TestimonialMapper;
 import com.alkemy.ong.models.request.TestimonialRequest;
+import com.alkemy.ong.models.response.TestimonialPageResponse;
 import com.alkemy.ong.models.response.TestimonialResponse;
 import com.alkemy.ong.repository.TestimonialRepository;
 import com.alkemy.ong.service.TestimonialService;
+import com.alkemy.ong.utils.ClassUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import static com.alkemy.ong.utils.ApiConstants.PATH_MEMBERS;
+import static com.alkemy.ong.utils.ApiConstants.PATH_TESTIMONIALS;
+import static org.springframework.data.support.PageableExecutionUtils.getPage;
+
 @Service
-public class TestimonialServiceImpl implements TestimonialService {
+public class TestimonialServiceImpl extends ClassUtil<TestimonialEntity, Long, TestimonialRepository> implements TestimonialService {
 
     @Autowired
     private TestimonialRepository testimonialRepository;
@@ -49,5 +57,16 @@ public class TestimonialServiceImpl implements TestimonialService {
         TestimonialEntity entity = testimonialRepository.findById(id)
                                     .orElseThrow(() -> new OrgNotFoundException("Testimonial not found"));
         testimonialRepository.delete(entity);
+    }
+
+    @Override
+    public TestimonialPageResponse pagination(Integer numberOfPage) {
+        if (numberOfPage < 1) throw new RuntimeException("Resource not found");
+
+        Page<TestimonialEntity> page = getPage(numberOfPage);
+        String previous = getPrevious(PATH_TESTIMONIALS, numberOfPage);
+        String next = getNext(page, PATH_TESTIMONIALS, numberOfPage);
+
+        return testimonialMapper.entityPage2PageResponse(page.getContent(), previous, next);
     }
 }
