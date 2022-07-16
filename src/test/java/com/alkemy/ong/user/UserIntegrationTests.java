ad1821bfbd8428d4175dacfea5f83e945fa5ebdc
Adrian <adrix.43@gmail.com>
2022-07-15 21:47:34 -0300

    starting tests

---
+++ src/test/java/com/alkemy/ong/user/UserIntegrationTests.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.user;
+
+import com.alkemy.ong.ContextTests;
+import org.junit.jupiter.api.Test;
+
+public class UserIntegrationTests extends ContextTests {
+
+    private final static String URL_CONTROLLER = "/users";
+
+    @Test
+    public void
+}
